40e989ce76eb257fa674d0c0d3ed270f22025d1b
StrategyMasta <79429259+StrategyMasta@users.noreply.github.com>
2021-06-10 01:07:47 +0200

    enemies V1
    Basic Enemy Movement

---
+++ enemies.js
@@ -0,0 +1,48 @@
+var enemySize={turret1: [41, 62]};
+var enemies=[];
+var enemiesInt;
+class Enemy{
+    constructor(type, baby){
+        this.type=type;
+        this.baby=baby;
+        this.x=0;
+        this.y=0;
+        if(!baby){
+            let random=Math.floor(Math.random()*4)+1;
+            if(random==1){
+                this.x=Math.floor(Math.random()*innerWidth)-enemySize[type][0]/2;
+                this.y=0-enemySize[type][1];
+            } else if(random==2){
+                this.x=innerWidth;
+                this.y=Math.floor(Math.random()*innerHeight)-enemySize[type][1]/2;
+            } else if(random==3){
+                this.x=Math.floor(Math.random()*innerWidth)-enemySize[type][0]/2;
+                this.y=innerHeight;
+            } else if(random==4){
+                this.x=0-enemySize[type][0];
+                this.y=Math.floor(Math.random()*innerHeight)-enemySize[type][1]/2;
+            }
+        }
+        else{
+            this.x=baby.x-enemySize[type][0];
+            this.y=baby.y-enemySize[type][1];
+        }
+        this.enemy=document.createElement("IMG");
+        document.body.appendChild(this.enemy);
+        this.enemy.setAttribute("src", "img/"+type+".png");
+        let angle=Math.atan2(innerHeight/2-this.y, innerWidth/2-this.x);
+        this.angle=(180/Math.PI*angle+90)%360;
+        this.enemy.style.transform="rotate("+this.angle+"deg)";
+        this.enemy.style.position="absolute";
+        this.enemy.style.top=this.y+"px";
+        this.enemy.style.left=this.x+"px";
+    }
+    moveToPlayer(){
+        enemies.forEach((item, index)=>{
+            item.x+=Math.cos(item.angle/(180/Math.PI)-Math.PI/2)*7;
+            item.y+=Math.sin(item.angle/(180/Math.PI)-Math.PI/2)*7;
+            item.enemy.style.top=item.y+"px";
+            item.enemy.style.left=item.x+"px";
+        });
+    }
+}
