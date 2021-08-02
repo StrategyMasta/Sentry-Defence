cd24f321456811b3dfe991b9606e49a4b5c9acec
StrategyMasta <79429259+StrategyMasta@users.noreply.github.com>
2021-08-03 00:38:35 +0200

    Menu Buttons Animation
    Hover Over The Menu Buttons And See What Happens!

--- upgrades.js
+++ upgrades.js
@@ -18,3 +18,64 @@ function upgrade(index){
     price[index].innerText=price[index].innerText*1+50;
     cost[index]+=50;
 }
+const particlesMenu=document.getElementById("particlesMenu");
+const particlesMenuCtx=particlesMenu.getContext("2d");
+const particleArray = [];
+const edge = 120;
+var selectedNode = document.getElementById("start");
+particlesMenu.width=300;
+particlesMenu.height=250;
+function particleHover(node){
+    selectedNode = node;
+    particlesMenu.style.visibility = "visible";
+    particlesMenu.style.top = (node.getBoundingClientRect().top + node.getBoundingClientRect().height/2 - 125) + "px";
+    particlesMenu.style.left = (node.getBoundingClientRect().left + node.getBoundingClientRect().width/2 - 150) + "px";
+    animate();
+}
+function particleOut(){
+    selectedNode = null;
+    particlesMenu.style.visibility = "hidden";
+    cancelAnimationFrame(animationLoop);
+    particlesMenuCtx.clearRect(-1, -1, particlesMenu.width + 2, particlesMenu.height + 2);
+}
+class Particle{
+    constructor(num){
+        this.x = 150;
+        this.y = 125;
+        this.speed = 1;
+        this.angle = Math.random() * Math.PI * 2;
+        this.strokeCol = num ? "black" : "white";
+        this.fillCol = num ? "white" : "black";
+    }
+    update(){
+        this.speed += .1;
+        this.x += Math.cos(this.angle) * this.speed;
+        this.y += Math.sin(this.angle) * this.speed;
+        this.draw();
+    }
+    draw(){
+        this.speed += .1;
+        this.x += Math.cos(this.angle) * this.speed;
+        this.y += Math.sin(this.angle) * this.speed;
+        
+        let direction = Math.sqrt((this.x - 150) ** 2 + (this.y - 125) ** 2);
+        let radius = (-direction / edge + 1) * edge / 10;
+        if(radius > 0){
+            requestAnimationFrame(this.draw.bind(this));
+            particlesMenuCtx.fillStyle = this.fillCol;
+            particlesMenuCtx.strokeStyle = this.strokeCol;
+            particlesMenuCtx.beginPath();
+            particlesMenuCtx.arc(this.x, this.y, radius, 0, Math.PI * 2);
+            particlesMenuCtx.fill();
+            particlesMenuCtx.stroke();
+        }
+    }
+}
+var animationLoop;
+function animate(){
+    particlesMenuCtx.fillStyle = "rgba(105, 105, 105, 0.15)";
+    particlesMenuCtx.fillRect(0, 0, particlesMenu.width, particlesMenu.height);
+    let particle = new Particle(Math.floor(Math.random() * 2));
+    particle.draw();
+    animationLoop = requestAnimationFrame(animate);
+}
